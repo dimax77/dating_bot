1c2801b8db2f995a0f287def644ea7ab9d4e1c9d
mockingb <m.1@bk.ru>
2025-04-14 20:20:48 +0300

    adds start functionality to display index.html

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from app.routes import main
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'supersecretkey'  # Лучше вынести в config
+    app.secret_key = 'supersecretkey'
     app.register_blueprint(main)
     return app
