a35279c3b8a33c084dedf5e61810e766aa960d78
mockingb <m.1@bk.ru>
2025-04-14 22:03:10 +0300

    adds init_db() inti __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from app.routes import main
+from app.routes import init_db
+
 
 def create_app():
+    init_db()
+
     app = Flask(__name__)
     app.secret_key = 'supersecretkey'
     app.register_blueprint(main)
     return app
+
