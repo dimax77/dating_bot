f114ab9a67ddc09d5388ef1eacfaa229f21becc9
mockingb <m.1@bk.ru>
2025-04-14 21:52:51 +0300

    adds create profile template, updates routes.py, create styles.css.

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,12 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
-@main.route('/create_profile', methods=['GET', 'POST'])
-def create_profile():
-    if request.method == 'POST':
-        # Обработка формы создания профиля
-        pass
-    return render_template('create_profile.html')
+# @main.route('/create_profile', methods=['GET', 'POST'])
+# def create_profile():
+#     if request.method == 'POST':
+#         # Обработка формы создания профиля
+#         pass
+#     return render_template('create_profile.html')
 
 @main.route('/search', methods=['GET', 'POST'])
 def search():
@@ -29,3 +29,34 @@ def result():
 def dialog(user_id):
     # Показать диалог с пользователем
     return render_template('dialog.html', user_id=user_id)
+
+# app/routes.py
+from flask import Blueprint, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+main = Blueprint('main', __name__)
+UPLOAD_FOLDER = 'static/uploads'
+
+@main.route('/create_profile', methods=['GET', 'POST'])
+def create_profile():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        city = request.form['city']
+        interests = request.form['interests']
+        about = request.form['about']
+        photo = request.files.get('photo')
+
+        photo_path = None
+        if photo and photo.filename:
+            filename = secure_filename(photo.filename)
+            photo_path = os.path.join(UPLOAD_FOLDER, filename)
+            photo.save(photo_path)
+
+        # 💾 TODO: сохранить данные в SQLite
+        print(f"Создан профиль: {name}, {age}, {city}, {interests}, {about}, {photo_path}")
+
+        return redirect(url_for('main.index'))
+
+    return render_template('create_profile.html')
