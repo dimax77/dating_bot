0881753756317d9063deb2df6d3cfcd72ae9c903
mockingb <m.1@bk.ru>
2025-04-15 14:50:35 +0300

    Some design updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from app.routes import main
 from app.routes import init_db
+import os
 
 
 def create_app():
     init_db()
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), '../static'),)
     app.secret_key = 'supersecretkey'
     app.register_blueprint(main)
     return app
---
+++ static/js/history.js
@@ -0,0 +1,26 @@
+// static/js/history.js
+window.addEventListener('DOMContentLoaded', () => {
+    // При первом запуске pushState, чтобы не выйти сразу из приложения
+    if (!window.location.hash) {
+        history.replaceState({ page: "home" }, "", window.location.pathname + "#home");
+    }
+
+    // Пример: если пользователь нажимает "назад"
+    window.addEventListener("popstate", (e) => {
+        if (!e.state || e.state.page === "home") {
+            // Закрыть приложение (если Telegram поддерживает)
+            if (window.Telegram.WebApp) {
+                window.Telegram.WebApp.close();
+            } else {
+                // Фолбэк
+                alert("Выход из приложения");
+            }
+        }
+    });
+});
+
+function navigateTo(event, url) {
+    event.preventDefault();
+    history.pushState({ page: url }, "", url);
+    window.location.href = url;
+}
