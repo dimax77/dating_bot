ba8c4adad9328216a418faf4f7af17f26935fa21
mockingb <m.1@bk.ru>
2025-04-19 21:48:11 +0300

    Some design updates

--- app/routes.py
+++ app/routes.py
@@ -6,11 +6,8 @@ import sqlite3
 
 from werkzeug.utils import secure_filename
 
-
-
 DB_PATH = 'data/profiles.db'
 # UPLOAD_FOLDER = 'app/static/uploads'
-
 
 main = Blueprint('main', __name__)
 UPLOAD_FOLDER = 'static/uploads'
@@ -19,22 +16,35 @@ UPLOAD_FOLDER = 'static/uploads'
 def index():
     return render_template('base.html', content_template="fragments/home.html", body_class="welcome")
 
-# @main.route('/search', methods=['GET', 'POST'])
-# def search():
-#     if request.method == 'POST':
-#         # Обработка формы поиска
-#         return redirect(url_for('main.result'))
-#     return render_template('search.html')
+@main.route('/create_profile', methods=['GET', 'POST'])
+def create_profile():
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        city = request.form['city']
+        interests = request.form['interests']
+        about = request.form['about']
+        photo = request.files.get('photo')
+        telegram_id = request.args.get('id')  # если мы получаем id через query params из Telegram
 
-@main.route('/result')
-def result():
-    # Показать результаты поиска
-    return render_template('result.html')
+        filename = None
+        if photo and photo.filename:
+            os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+            filename = secure_filename(photo.filename)
+            photo.save(os.path.join(UPLOAD_FOLDER, filename))
 
-# @main.route('/dialog/<int:user_id>')
-# def dialog(user_id):
-#     # Показать диалог с пользователем
-#     return render_template('dialog.html', user_id=user_id)
+        # 💾 сохраняем профиль
+        conn = sqlite3.connect(DB_PATH)
+        cur = conn.cursor()
+        cur.execute('''
+            INSERT INTO profiles (name, age, city, interests, about, photo, telegram_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        ''', (name, age, city, interests, about, filename, telegram_id))
+        conn.commit()
+        conn.close()
+        return render_template('base.html', content_template='fragments/home.html', message="Анкета успешно создана!")
+        # return redirect(url_for('main.index'))
+    return render_template('base.html', content_template='fragments/create_profile.html')
 
 
 @main.route('/edit_profile', methods=['GET', 'POST'])
@@ -146,34 +156,4 @@ def init_db():
     ''')
     conn.commit()
     conn.close()
-
-@main.route('/create_profile', methods=['GET', 'POST'])
-def create_profile():
-    if request.method == 'POST':
-        name = request.form['name']
-        age = request.form['age']
-        city = request.form['city']
-        interests = request.form['interests']
-        about = request.form['about']
-        photo = request.files.get('photo')
-        telegram_id = request.args.get('id')  # если мы получаем id через query params из Telegram
-
-        filename = None
-        if photo and photo.filename:
-            os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-            filename = secure_filename(photo.filename)
-            photo.save(os.path.join(UPLOAD_FOLDER, filename))
-
-        # 💾 сохраняем профиль
-        conn = sqlite3.connect(DB_PATH)
-        cur = conn.cursor()
-        cur.execute('''
-            INSERT INTO profiles (name, age, city, interests, about, photo, telegram_id)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (name, age, city, interests, about, filename, telegram_id))
-        conn.commit()
-        conn.close()
-        return render_template('base.html', content_template='fragments/home.html', message="Анкета успешно создана!")
-        # return redirect(url_for('main.index'))
-    return render_template('base.html', content_template='fragments/create_profile.html')
 
