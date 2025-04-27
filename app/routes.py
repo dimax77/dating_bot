32ac9f3d4b2b2ca9c3d2476f4779d2b7a45884aa
mockingb <m.1@bk.ru>
2025-04-27 22:04:37 +0300

    Some design updates

--- app/routes.py
+++ app/routes.py
@@ -31,10 +31,10 @@ def index():
     if user_id:
         try:
             user_data = get_user_by_telegram_id(user_id)
-            current_app.logger.info("Found user: %s", user_data)
             if user_data:
                 user = dict(user_data)
-                unread_count = get_unread_messages_count(user_data.id)
+                current_app.logger.info("Found user: %s", user)
+                unread_count = get_unread_messages_count(user['id'])
             else:
                 # If user doesn't exist, clear session and redirect
                 # session.clear()  # Remove all session data
